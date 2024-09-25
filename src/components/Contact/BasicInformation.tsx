bb729d38ba072c06949b28754e2987f89599e18f
James Chan <jameschan312.cn@gmail.com>
2024-09-24 23:05:49 -0400

    hid the scroll bar

--- src/components/Contact/BasicInformation.tsx
+++ src/components/Contact/BasicInformation.tsx
@@ -3,10 +3,14 @@ import { Alert, Box, Typography } from '@mui/material'
 export function BasicInformation() {
     return (
         <Box>
-            <Alert severity="info">This section is currently under construction.</Alert>
-            <Typography>Legal Name: Zhuojian Chen (陈卓坚/陳卓堅)</Typography>
-            <Typography>Preferred First Name: James</Typography>
-            <Typography>Email Address: jameschan312.cn@gmail.com</Typography>
+            <Alert severity="info" sx={{ marginBottom: '1rem' }}>
+                This section is currently under construction.
+            </Alert>
+            <Box>
+                <Typography>Legal Name: Zhuojian Chen (陈卓坚/陳卓堅)</Typography>
+                <Typography>Preferred First Name: James</Typography>
+                <Typography>Email Address: jameschan312.cn@gmail.com</Typography>
+            </Box>
         </Box>
     )
 }
